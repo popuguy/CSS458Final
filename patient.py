3a7204691eee2b80eb09491c46a614fdcf981463
Your Name <you@example.com>
2020-03-03 14:37:02 -0800

    Add patient normal

--- patient.py
+++ patient.py
@@ -4,6 +4,13 @@ from patient_constant import *
 
 class Patient:
     def __init__(self):
+        """For now, Patient initialization should always be randomized
+        according to data-based attributes of hospital patients from
+        Karaca et al.
+
+        Source:
+        https://bmcemergmed.biomedcentral.com/track/pdf/10.1186/1471-227X-12-15
+        """
         self.status = PatientStatus.UNQUEUED
         self.time_queued = None
         self._give_statistical_attributes()
