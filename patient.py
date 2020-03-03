4a8fdbdd18fa90d0825b3dee45e9527966cd3d0a
Your Name <you@example.com>
2020-03-03 14:34:38 -0800

    Add patient status and queue time

--- patient.py
+++ patient.py
@@ -1,9 +1,15 @@
 import random
+from patient_constant import *
 
 
 class Patient:
     def __init__(self):
+        self.status = PatientStatus.UNQUEUED
+        self.time_queued = None
         self._give_statistical_attributes()
 
     def _give_statistical_attributes(self):
         self.age = random.choice(10, 20, 30)
+
+    def queue(self, queue_time):
+        self.time_queued = queue_time
--- patient_constant.py
+++ patient_constant.py
@@ -6,7 +6,15 @@ Created on Mon Mar  2 20:55:52 2020
 @author: minhuyen, popuguy
 """
 import numpy as np
+from enum import Enum
 import random as rand
+
+
+class PatientStatus(Enum):
+    UNQUEUED = 0
+    WAITING = 1
+    IN_ROOM = 2
+    EXITING = 3
 
 
 class PatientConstant:
