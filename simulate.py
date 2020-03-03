fbae9fd59186747b50746f4bea67e89687a841a0
Your Name <you@example.com>
2020-03-03 13:32:29 -0800

    Add hospital constant and minimal simulate file opening

---
+++ simulate.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+import datetime
+import hospital_constant
+
+def simulate_waiting(time_span=timedelta(days=1),
+                     time_delta=timedelta(minutes=3)):
+    exam_rooms = []
+
+
+if __name__ == '__main__':
+    simulate_waiting()
