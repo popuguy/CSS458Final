48838a7a267cef8c951925cca6b9f4e98a729bc9
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-02 22:58:13 -0800

    Add files via upload

---
+++ patientConstant.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 20:55:52 2020
+
+@author: minhuyen
+"""
+import numpy as np
+import random as rand
+
+class patient_constant:
+    AGE = np.range(10, 50, 1)
+    GENDER = ["Male", "Female", "Prefer not to answer"]
+    RACE = ["American", "Asian", "African"]
+    
