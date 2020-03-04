de50c2382d1eaaa7ce69aa8c8f5f54c3dfaae9b1
mxng113 <60676926+mxng113@users.noreply.github.com>
2020-03-04 10:12:22 -0800

    Update patient_working.py

--- patient_working.py
+++ patient_working.py
@@ -25,10 +25,28 @@ class Patient:
         self.levelOfUrgency = random.randint(0, 9) 
         
         # The time a patient spend with a doctor
-        if(self.gender == "Male"):
-            self.consutation_time = random.normalvarities(patient_constant.MALE_MEAN, 0.0)
+        self.consutation_time = patient_constant.MEAN_ALL_VISITS
+        
+        if(self.gender == "Male"):      #- Gender attribute
+            self.consutation_time *= patient_constant.RATE_GENDER_MALE
         elif(self.gender == "Female"):
-            self.consutation_time = random.normalvarities(patient_constant.FEMALE_MEAN, 0.0)
+            self.consutation_time *= patient_constant.RATE_GENDER_FEMALE
+            
+        if (self.race == "White"):      #- Race attribute
+            self.consutation_time *= patient_constant.RATE_RACE_WHITE
+        elif (self.race == "Black"):
+            self.consutation_time *= patient_constant.RATE_RACE_BLACK
+        elif (self.race == "Hispanic):
+            self.consutation_time *= patient_constant.RATE_RACE_HISPANIC
+        elif (self.race == "Asian"):
+            self.consutation_time *= patient_constant.RATE_RACE_ASIAN
+        elif (self.race == "Native"):
+            self.consutation_time *= patient_constant.RATE_RACE_NATIVE
+        else:
+            self.consutation_time *= patient_constant.RATE_RACE_OTHER
+        
+        
+        
         self.enter_room_time = environment.now # The time a patient enters the examination room
         self.waiting_time = 0 # The time a patient waits to see a doctor
         self.exit_ER = 0 # The time a patient exits the examination room
