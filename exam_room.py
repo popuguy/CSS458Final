75af71f6de10d8554dc38e6b3f3071248fafb48e
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-03 22:15:19 -0800

    Update exam_room.py

--- exam_room.py
+++ exam_room.py
@@ -4,3 +4,4 @@ from patient import Patient
 class ExamRoom:
     def __init__(self):
         self.patient = None  # None, if no patient occupying
+        self.doctor = None #should we have doctor too?
