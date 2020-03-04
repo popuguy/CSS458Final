dc502697e944d53089c2f078850f7102db352153
mxng113 <60676926+mxng113@users.noreply.github.com>
2020-03-04 10:14:32 -0800

    Update patient_constant.py

--- patient_constant.py
+++ patient_constant.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  2 20:55:52 2020
-
-@author: minhuyen, popuguy
-"""
 import numpy as np
 from enum import Enum
 import random as rand
@@ -18,6 +11,27 @@ class PatientStatus(Enum):
 
 
 class PatientConstant:
+    """
+    Calculating the rate difference from the mean of all visits' time duration
+    of difference attributes 
+    
+    """
+    MEAN_ALL_VISITS = 195.7     #- mean duration of 
+    
+    RATE_GENDER_MALE = 187.4 / MEAN_ALL_VISITS
+    RATE_GENDER_FEMALE = 202.8 / MEAN_ALL_VISITS
+    
+    RATE_RACE_WHITE = 190.6 / MEAN_ALL_VISITS
+    RATE_RACE_BLACK = 212.0 / MEAN_ALL_VISITS
+    RATE_RACE_HISPANIC = 202.4 / MEAN_ALL_VISITS
+    RATE_RACE_ASIAN = 203.8 / MEAN_ALL_VISITS
+    RATE_RACE_NATIVE = 204.7 / MEAN_ALL_VISITS
+    RATE_RACE_OTHER = 193.8 / MEAN_ALL_VISITS
+    
+    
+    
     AGE = np.range(10, 50, 1)
-    GENDER = ["Male", "Female", "Prefer not to answer"]
-    RACE = ["American", "Asian", "African"]
+    GENDER = ["Male", "Female"]
+    RACE = ["White", "Asian", "Black", "Hispanic", "Native", "Other"]
+    
+    
