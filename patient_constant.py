1dfed2fb225d69aad6b0fb5b02d4cf181349e3a0
mxng113 <60676926+mxng113@users.noreply.github.com>
2020-03-04 23:07:16 -0800

    Update patient_constant.py

--- patient_constant.py
+++ patient_constant.py
@@ -18,9 +18,12 @@ class PatientConstant:
     """
     MEAN_ALL_VISITS = 195.7     #- mean duration of 
     
+    #- Calculating different rate of visit duration for each attribute
+    #- Patient's gender attribute
     RATE_GENDER_MALE = 187.4 / MEAN_ALL_VISITS
     RATE_GENDER_FEMALE = 202.8 / MEAN_ALL_VISITS
     
+    #- Patient's race attribute
     RATE_RACE_WHITE = 190.6 / MEAN_ALL_VISITS
     RATE_RACE_BLACK = 212.0 / MEAN_ALL_VISITS
     RATE_RACE_HISPANIC = 202.4 / MEAN_ALL_VISITS
@@ -28,10 +31,16 @@ class PatientConstant:
     RATE_RACE_NATIVE = 204.7 / MEAN_ALL_VISITS
     RATE_RACE_OTHER = 193.8 / MEAN_ALL_VISITS
     
+    #- Patient's insurance coverage attribute
+    RATE_INSURANCE_MEDICARE = 237.7 / MEAN_ALL_VISITS
+    RATE_INSURANCE_MEDICAID = 182.8 / MEAN_ALL_VISITS
+    RATE_INSURANCE_PRIVATE = 192.8 / MEAN_ALL_VISITS
+    RATE_INSURANCE_OTHER = 169.4 / MEAN_ALL_VISITS
+    RATE_INSURANCE_UNINSURED = 191.8 / MEAN_ALL_VISITS
     
     
     AGE = np.range(10, 50, 1)
     GENDER = ["Male", "Female"]
     RACE = ["White", "Asian", "Black", "Hispanic", "Native", "Other"]
-    
+    INSURANCE = ["Medicare", "Medicaid", "Private", "Other", "Uninsured"]
     
