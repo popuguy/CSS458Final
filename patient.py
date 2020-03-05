5276fc22c7fb62aed46892dbcde0066b79afd7e5
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-05 14:10:10 -0800

    Update patient.py

--- patient.py
+++ patient.py
@@ -1,10 +1,15 @@
+# Import statements
 import random
 from patient_constant import *
 import numpy as np
 from datetime import timedelta
 
-
+"""This class represents a patient with these attributes: sex, age, race, and insurance status. A patient will aslo have a 
+    variable called status to keep track of where they are in the queue.
+"""
 class Patient:
+    
+    # Constructor for the class
     def __init__(self):
         """For now, Patient initialization should always be randomized
         according to data-based attributes of hospital patients from
@@ -13,17 +18,18 @@ class Patient:
         Source:
         https://bmcemergmed.biomedcentral.com/track/pdf/10.1186/1471-227X-12-15
         """
-        self.status = PatientStatus.UNQUEUED
-        self.time_queued = None
-        self.time_served = None
-        self.time_exited = None
+        self.status = PatientStatus.UNQUEUED # keep track of where a patient is in a queue
+        self.time_queued = None  # the time when a patient is in the queue
+        self.time_served = None  # the time when a patient is served, or enter the exam room
+        self.time_exited = None  # the time when a patient is done with the examination, and leave the ER
         # Once a patient has actually been seen by a doctor this changes to
         # True. It is a condition for exiting
         self.seen_by_doctor = False
-        self._give_statistical_attributes()
+        self._give_statistical_attributes()  # generate random attributes to a patient
 
     def _give_statistical_attributes(self):
-        # self.age = random.choice(10, 20, 30)
+        """Assign random value to a patient's attributes
+        """
         # Possible information on a statistical way to use the multiple means
         # for different attributes:
         # https://www.researchgate.net/post/Is_there_a_statistical_procedure_for_comparing_multiple_means_to_a_specific_value
@@ -32,6 +38,7 @@ class Patient:
         self.sex = np.random.choice([PatientSex.MALE, PatientSex.FEMALE],
                                     p=[PatientConstant.SOURCE_DATA_PORTION_MALE,
                                        1 - PatientConstant.SOURCE_DATA_PORTION_MALE])
+        
         self.age = np.random.choice([PatientAge.AGE_UNDER_15,
                                      PatientAge.AGE_15_24,
                                      PatientAge.AGE_25_44,
@@ -44,6 +51,7 @@ class Patient:
                                        PatientConstant.SOURCE_DATA_PORTION_AGE_45_64,
                                        PatientConstant.SOURCE_DATA_PORTION_AGE_65_74,
                                        PatientConstant.SOURCE_DATA_PORTION_AGE_OVER_74])
+        
         self.race = np.random.choice([PatientRace.WHITE,
                                       PatientRace.BLACK,
                                       PatientRace.HISPANIC,
@@ -56,6 +64,7 @@ class Patient:
                                         PatientConstant.SOURCE_DATA_PORTION_RACE_ASIAN,
                                         PatientConstant.SOURCE_DATA_PORTION_RACE_NATIVE,
                                         PatientConstant.SOURCE_DATA_PORTION_RACE_OTHER])
+        
         self.insurance = np.random.choice([PatientInsurance.MEDICARE,
                                            PatientInsurance.MEDICAID,
                                            PatientInsurance.PRIVATE,
@@ -66,7 +75,7 @@ class Patient:
                                              PatientConstant.SOURCE_DATA_PORTION_INSURANCE_PRIVATE,
                                              PatientConstant.SOURCE_DATA_PORTION_INSURANCE_OTHER,
                                              PatientConstant.SOURCE_DATA_PORTION_INSURANCE_UNINSURED])
-
+        # find the mean of the waiting time
         self.calculated_mean_hospital_time = self._calc_mean_wait_time()
         # self.actual_hospital_time_min = something...
 
@@ -75,18 +84,26 @@ class Patient:
         return PatientConstant.MEAN_ALL_VISITS
 
     def queue(self, queue_time):
+        """Enqueue a patient
+        """
         self.status = PatientStatus.WAITING
         self.time_queued = queue_time
 
     def serve(self, service_time):
+        """Serve a patient by moving them into the exam room
+        """
         self.status = PatientStatus.IN_ROOM
         self.time_served = service_time
 
     def exit(self, cur_time):
+        """Remove a patient from the exam room and get their departure time
+        """
         self.time_exited = cur_time
         self.status = PatientStatus.EXITING
 
     def has_completed_visit(self, cur_time):
+        """Get the total time during a patient's visit by adding waiting time and consultation time
+        """
         return (cur_time <= self.time_served +
                 timedelta(
                     minutes=self.calculated_mean_hospital_time)) and \
