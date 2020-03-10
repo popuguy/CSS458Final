eb6c2c2a0a7db5b0abdb7133f703b6841e840ce8
Your Name <you@example.com>
2020-03-10 15:53:31 -0700

    Add linear inc/dec queueing style with new constants

--- entrance_styles.py
+++ entrance_styles.py
@@ -6,7 +6,7 @@ class PatientEntranceStyles:
     def __init__(self):
         self.patients_to_generate = 0.0
 
-    def basic_entrance(self, time_delta,
+    def basic(self, time_delta,
                        patients_per_hour=HospitalConstant.PATIENTS_PER_HOUR):
         """
 
@@ -26,3 +26,11 @@ class PatientEntranceStyles:
         self.patients_to_generate -= int(self.patients_to_generate)
 
         return num_new_generate
+
+    def peak_and_fall_linear(self, num_loops, time_delta,
+                             time_from_start_peak=timedelta(hours=12),
+                             peak_num_patients_per_hour=
+                             HospitalConstant.DEFAULT_PEAK_PATIENTS,
+                             min_num_patients_per_hour=
+                             HospitalConstant.DEFAULT_MIN_PATIENTS):
+        pass
--- hospital_constant.py
+++ hospital_constant.py
@@ -7,3 +7,8 @@ class HospitalConstant:
 
     # Constant to modify for different simulations
     PATIENTS_PER_HOUR = 10
+
+    # Constants for linear inc/dec style patient entrance
+    # Reference data: https://www.beckershospitalreview.com/quality/how-healthy-is-your-emergency-department.html
+    DEFAULT_PEAK_PATIENTS = 13
+    DEFAULT_MIN_PATIENTS = 1.8
